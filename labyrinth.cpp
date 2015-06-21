61d2824389f75c3a953971de93ec13cde2b50e06
Giovanni <Giovanni@GBrunetti.homenet.telecomitalia.it>
2015-06-21 03:39:36 +0200

    Completamento luci

--- labyrinth.cpp
+++ labyrinth.cpp
@@ -8,12 +8,13 @@
 #include <ctime>
 #include "iostream"
 using namespace std;
-int h = 5; //altezza muri
 int c = 6; //determina la grandezza del labirinto
+const int h = c / 2; //altezza muri
 const int confine = 1.8;
 int saveX, saveY;
 int length = c * 36; //lunghezza matrice
 int width = c * 29; //larghezza matrice
+const int s = 1.5;
 float a = 0.0; //angolo
 float da = 1;
 float l = 0.6;
@@ -21,17 +22,23 @@ float d = 1.3; //dimensione cubo rotante
 bool aerial = false; //segnala se la visuale e dall'alto
 float rotate = 0;
 float spec[] =
-{ 1.0, 1.0, 1.0, 1.0 };
+{ 0.5, 0.5, 0.5, 1.0 };
 float sheen[] =
-{ 25.0 };
-float lightPosition[] =
-{ (float)width, (float)length, 20.0, 5.0 };
+{ 20.0 };
+float lightPosition1[] =
+{ (float) width, (float) length, 6.0, 5.0 };
+float lightPosition2[] =
+{ 0, (float) length, 6.0, 5.0 };
+float lightPosition3[] =
+{ 0, 0, 6.0, 5.0 };
+float lightPosition4[] =
+{ (float) width, 0, 6.0, 5.0 };
 float light[] =
-{ 1.0, 1.0, 1.0, 1.0 };
-float ambiente[] =
-{ 0.6, 0.6, 0.6, 1.0 };
+{ 0.8, 0.8, 0.8, 0.7 };
+float environment[] =
+{ 0.9, 0.6, 0.6, 1.0 };
 float floorColor[] =
-{ 0.9, 0.9, 0.9 };
+{ 0.4, 0.4, 0.4 };
 float ceilingColor[] =
 { 1.0, 1.0, 1.0 };
 float OutWallColor[] =
@@ -101,7 +108,6 @@ void aerialWiew()
 {
 	saveX = vettori.eye[0];
 	saveY = vettori.eye[1];
-	h = 1;
 	c = 2;
 	aerial = true;
 	length = c * 36;
@@ -113,14 +119,13 @@ void aerialWiew()
 }
 void standardWiew()
 {
-	h = 5;
 	c = 6;
 	length = c * 36;
 	width = c * 29;
 	aerial = false;
 	vettori.eye[0] = saveX;
 	vettori.eye[1] = saveY;
-	vettori.eye[2] = 0.88;
+	vettori.eye[2] = 1.3;
 	d = 1.3;
 }
 void verify(int&x, int&y)
@@ -148,26 +153,25 @@ void initializateVision()
 	verify(x, y);
 	vettori.eye[0] = x;
 	vettori.eye[1] = y;
-	vettori.eye[2] = 0.88;
+	vettori.eye[2] = 1.3;
 	vettori.direction[0] = 1;
-	vettori.direction[1] = 0;
+	vettori.direction[1] = 1;
 	vettori.direction[2] = 1;
 	vettori.up[0] = 0;
 	vettori.up[1] = 0;
-	vettori.up[2] = 2;
+	vettori.up[2] = 3;
 
 }
 void reset()
 {
-	h = 7;
-	c = 8;
+	c = 6;
 	aerial = true;
 	initializateVision();
 }
 bool goForwardIn()
 {
-	int x = (int) ((vettori.eye[0] / c) + l * cos((a * 3.14) / 180));
-	int y = (int) ((vettori.eye[1] / c) + l * sin((a * 3.14) / 180));
+	int x = (int) (((vettori.eye[0] + s + 1) / c) + l * cos((a * 3.14) / 180));
+	int y = (int) (((vettori.eye[1] + s + 1) / c) + l * sin((a * 3.14) / 180));
 	if (map[x][y] == 1)
 	{
 		cout << "conflict with wall" << endl;
@@ -177,8 +181,8 @@ bool goForwardIn()
 }
 bool goBackwardIn()
 {
-	int x = (int) ((vettori.eye[0] / c) - l * cos((a * 3.14) / 180));
-	int y = (int) ((vettori.eye[1] / c) - l * sin((a * 3.14) / 180));
+	int x = (int) (((vettori.eye[0] + s + 1) / c) - l * cos((a * 3.14) / 180));
+	int y = (int) (((vettori.eye[1] + s + 1) / c) - l * sin((a * 3.14) / 180));
 	if (map[x][y] == 1)
 	{
 		cout << "conflict with wall" << endl;
@@ -208,14 +212,17 @@ bool goBackwardOut()
 }
 void init()
 {
-	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition1);
+//	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition2);
+//	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition3);
+//	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition4);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, light);
 	glLightfv(GL_LIGHT0, GL_SPECULAR, light);
-	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambiente);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glClearColor(1, 1, 1, 0.0);
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, environment);
+	glEnable(GL_LIGHTING); //abilita luce ambiente
+	glEnable(GL_LIGHT0); //abilita luci
 	glShadeModel(GL_SMOOTH);
+	glClearColor(1, 1, 1, 0.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glEnable(GL_DEPTH_TEST);
@@ -287,10 +294,10 @@ void drawFloor()
 	glPushMatrix();
 	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, floorColor);
 	glBegin(GL_QUADS);
-	glVertex3f(width, 0, 0);
-	glVertex3f(width, length, 0);
-	glVertex3f(0, length, 0);
-	glVertex3f(0, 0, 0);
+	glVertex3f(width, 0, -h);
+	glVertex3f(width, length, -h);
+	glVertex3f(0, length, -h);
+	glVertex3f(0, 0, -h);
 	glEnd();
 	glPopMatrix();
 }
@@ -306,58 +313,29 @@ void drawCeiling()
 	glEnd();
 	glPopMatrix();
 }
-void drawinteriorWall(int i, int j)
-{
-	i = i * c;
-	j = j * c;
-	int ic = i + c;
-	int jc = j + c;
-	glBegin(GL_QUADS);
-	glVertex3f(jc, ic, h);
-	glVertex3f(j, ic, h);
-	glVertex3f(j, i, h);
-	glVertex3f(jc, i, h);
-	glVertex3f(jc, ic, h);
-	glVertex3f(j, ic, h);
-	glVertex3f(j, ic, 0);
-	glVertex3f(jc, ic, 0);
-	glVertex3f(j, ic, 0);
-	glVertex3f(j, ic, h);
-	glVertex3f(j, i, h);
-	glVertex3f(j, i, 0);
-	glVertex3f(jc, i, 0);
-	glVertex3f(j, i, 0);
-	glVertex3f(j, i, h);
-	glVertex3f(jc, i, h);
-	glVertex3f(jc, ic, h);
-	glVertex3f(jc, ic, 0);
-	glVertex3f(jc, i, 0);
-	glVertex3f(jc, i, h);
-	glEnd();
-}
 void drawOuterWall()
 {
 	glBegin(GL_QUADS);
-	glVertex3f(0, 0, 0);
-	glVertex3f(0, length, 0);
+	glVertex3f(0, 0, -h);
+	glVertex3f(0, length, -h);
 	glVertex3f(0, length, h);
 	glVertex3f(0, 0, h);
 	glEnd();
 	glBegin(GL_QUADS);
-	glVertex3f(width, length, 0);
-	glVertex3f(width, 0, 0);
+	glVertex3f(width, length, -h);
+	glVertex3f(width, 0, -h);
 	glVertex3f(width, 0, h);
 	glVertex3f(width, length, h);
 	glEnd();
 	glBegin(GL_QUADS);
-	glVertex3f(width, 0, 0);
-	glVertex3f(0, 0, 0);
+	glVertex3f(width, 0, -h);
+	glVertex3f(0, 0, -h);
 	glVertex3f(0, 0, h);
 	glVertex3f(width, 0, h);
 	glEnd();
 	glBegin(GL_QUADS);
-	glVertex3f(width, length, 0);
-	glVertex3f(0, length, 0);
+	glVertex3f(width, length, -h);
+	glVertex3f(0, length, -h);
 	glVertex3f(0, length, h);
 	glVertex3f(width, length, h);
 	glEnd();
@@ -366,19 +344,15 @@ void positionRotatingCube(int i, int j)
 {
 	glPushMatrix();
 	glColor3f(1, 0, 0);
-	glTranslatef((i * c) + 1.5, (j * c) + 1.5, 1.5);
-	glRotatef(rotate, 0, 0, 5);
+	glTranslatef((i * c) + s, (j * c) + s, s);
+	glRotatef(rotate, 0, 0, s);
 	glutSolidCube(d);
 	glPopMatrix();
 	glColor3f(0, 1, 0);
 }
 void display(void)
 {
-//	float fogColor[] =
-//	{ 0.1, 0.1, 0.1, 1.0 };
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//	glFogfv(GL_FOG_COLOR, fogColor);
-//	glFogi(GL_FOG_MODE, GL_LINEAR);
 	glMatrixMode(GL_MODELVIEW);
 	glPushMatrix();
 	gluLookAt(vettori.eye[0], vettori.eye[1], vettori.eye[2],
@@ -392,7 +366,6 @@ void display(void)
 	drawOuterWall();
 	glPopMatrix();
 	glColor3f(0, 1, 0);
-	glPushMatrix();
 	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, InWallColor);
 	glMaterialfv(GL_FRONT, GL_SPECULAR, spec);
 	glMaterialfv(GL_FRONT, GL_SHININESS, sheen);
@@ -401,8 +374,12 @@ void display(void)
 		for (int j = 0; j < 36; j++)
 		{
 			if (map[i][j] == 1)
-				drawinteriorWall(j, i);
-			else if (map[i][j] == 2)
+			{
+				glPushMatrix();
+				glTranslatef((i * c) + s, (j * c) + s, 0);
+				glutSolidCube(c);
+				glPopMatrix();
+			} else if (map[i][j] == 2)
 			{
 				glPushMatrix();
 				glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, CubeColor);
@@ -414,7 +391,6 @@ void display(void)
 			}
 		}
 	}
-	glPopMatrix();
 	glPopMatrix();
 	glutSwapBuffers();
 }
@@ -431,11 +407,11 @@ void rotateCube(int value)
 int main(int argc, char **argv)
 {
 	loadMap();
-	initializateVision();
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(1200, 500);
 	glutCreateWindow("IG-Project");
+	initializateVision();
 	init();
 	glutKeyboardFunc(keyPressed);
 	glutReshapeFunc(reshape);
