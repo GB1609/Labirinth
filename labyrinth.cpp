976a75c26fa856557e860d92a89065e7b409b12a
Giovanni <Giovanni@GBrunetti>
2015-07-08 17:55:00 +0200

    progetto finito

--- labyrinth.cpp
+++ labyrinth.cpp
@@ -16,35 +16,32 @@ GLfloat normalFront[3]={1,1,0};
 GLfloat normalRight[3]={-1,1,0};
 GLfloat normalLeft[3]={1,-1,0};
 GLfloat normalBack[3]={-1,-1,0};
-float c=5;  //determina la grandezza del labirinto
+float c=6;  //determina la grandezza del labirinto
 const float h=c/2;  //meta diametro del cubo
-float saveX,saveY;
 float length=c*38;  //lunghezza matrice
 float width=c*31;  //larghezza matrice
 float a=0.0;  //angolo
 float da=1;
 float l=0.7;
-float d=1.3;  //dimensione cubo rotante
 float rimb=1.1;
 bool aerial=false;  //segnala se la visuale e dall'alto
 float rotate=0;
 float spec[]={0.5,0.5,0.5,1.0};
 float sheen[]={20.0};
-float lightPosition1[]={width-10,length-10,15.0,5.0};
+float lightPosition1[]={width,length,10.0,5.0};
 //float lightPosition2[]={0,length,6.0,5.0};
 //float lightPosition3[]={0,0,6.0,5.0};
 //float lightPosition4[]={width,0,6.0,5.0};
 float light[]={0.8,0.8,0.8,0.7};
-float environment[]={0.7,0.5,0.5,1.0};
-float CubeColor[]={1.0,0.0,0.0};
-float CubeColor1[]={0.0,0.0,1.0};
-const int numTextures=5;
+float environment[]={0.7,0.7,0.7,1.0};
+const int numTextures=9;
 GLuint textures[numTextures];
 const char *textureFile[numTextures]={"texture/ceiling.tga","texture/cube.tga","texture/floor.tga","texture/inWall.tga",
-		"texture/outWall.tga"};
+		"texture/outWall.tga","texture/player.tga","texture/sphere.tga","texture/fire.tga","texture/floorFire.tga"};
 int map[31][38];
 bool lost=false;
 bool win=false;
+float sphereDim=1.5;
 bool profVuoleVincere=false;
 struct VectorLookAt  //gestiscono la lookat
 {
@@ -90,7 +87,23 @@ generateXY(int div)
 {
 	srand (time (0));
 	int random=rand ()%div;
-	return random+1;
+	return random;
+}
+void
+zoom(bool operation)
+{
+	vectorLookAt.eye[0]/=c;
+	vectorLookAt.eye[1]/=c;
+	if (operation&&c<15)
+		c+=1;
+	else if (c>1)
+		c-=1;
+	length=c*38;
+	width=c*31;
+	vectorLookAt.eye[0]*=c;
+	vectorLookAt.eye[1]*=c;
+	vectorLookAt.direction[0]=vectorLookAt.eye[0]+cos ((a*3.14)/180);
+	vectorLookAt.direction[1]=vectorLookAt.eye[1]+sin ((a*3.14)/180);
 }
 void
 loadMap(int random)
@@ -139,49 +152,52 @@ loadMap(int random)
 void
 aerialWiew()
 {
-	saveX=vectorLookAt.eye[0];
-	saveY=vectorLookAt.eye[1];
+	vectorLookAt.eye[0]/=c;
+	vectorLookAt.eye[1]/=c;
 	c=2;
 	aerial=true;
 	length=c*38;
 	width=c*31;
-	vectorLookAt.eye[0]=width/2;
-	vectorLookAt.eye[1]=length/2;
+	if (!win&&!lost)
+	{
+		vectorLookAt.eye[0]*=c;
+		vectorLookAt.eye[1]*=c;
+	}
+	else
+	{
+		vectorLookAt.eye[0]=length/2;
+		vectorLookAt.eye[1]=width/2;
+	}
+	vectorLookAt.direction[0]=vectorLookAt.eye[0]+cos ((a*3.14)/180);
+	vectorLookAt.direction[1]=vectorLookAt.eye[1]+sin ((a*3.14)/180);
 	vectorLookAt.eye[2]=90;
-	d=0.8;
 }
 void
 standardWiew()
 {
-	c=5;
+	vectorLookAt.eye[0]/=c;
+	vectorLookAt.eye[1]/=c;
+	c=6;
 	length=c*38;
 	width=c*31;
 	aerial=false;
-	vectorLookAt.eye[0]=saveX;
-	vectorLookAt.eye[1]=saveY;
-	vectorLookAt.eye[2]=1.20;
-	d=1.3;
+	vectorLookAt.eye[0]*=c;
+	vectorLookAt.eye[1]*=c;
+	vectorLookAt.direction[0]=vectorLookAt.eye[0]+cos ((a*3.14)/180);
+	vectorLookAt.direction[1]=vectorLookAt.eye[1]+sin ((a*3.14)/180);
+	vectorLookAt.eye[2]=1.1;
 }
 void
 verify(int& x,int& y,int& posX,int& posY)
 {
 	if (map[x+1][y]==0)
-	{
 		posX+=c;
-	}
 	else if (map[x][y+1]==0)
-	{
 		posY+=c;
-
-	}
 	else if (map[x-1][y]==0)
-	{
 		posX-=c;
-	}
 	else if (map[x][y-1]==0)
-	{
 		posY-=c;
-	}
 }
 void
 initializateVision()
@@ -198,15 +214,13 @@ initializateVision()
 	verify (x,y,posX,posY);
 	vectorLookAt.eye[0]=posX;
 	vectorLookAt.eye[1]=posY;
-	vectorLookAt.eye[2]=1.20;
+	vectorLookAt.eye[2]=1.10;
 	vectorLookAt.direction[0]=vectorLookAt.eye[0]+cos ((3.14*a)/180);
 	vectorLookAt.direction[1]=vectorLookAt.eye[1]+sin ((3.14*a)/180);
 	vectorLookAt.direction[2]=1;
 	vectorLookAt.up[0]=0;
 	vectorLookAt.up[1]=0;
 	vectorLookAt.up[2]=3;
-	if (lost)
-		aerialWiew ();
 }
 void
 gameOver(int value)
@@ -217,12 +231,17 @@ gameOver(int value)
 		loadMap (5);
 	else if (lost)
 		loadMap (4);
-	initializateVision ();
+	aerialWiew ();
 	glutPostRedisplay ();
 }
 bool
 moving(int x,int y)
 {
+	if (map[x][y]==3)
+	{
+		cout<<"confict with OutWall"<<endl;
+		return false;
+	}
 	if (map[x][y]==1&&!profVuoleVincere)
 	{
 		cout<<"conflict with wall"<<endl;
@@ -249,7 +268,7 @@ init()
 	glEnable (GL_LIGHTING);  //abilita luce ambiente
 	glEnable (GL_LIGHT0);  //abilita luci
 	glShadeModel (GL_SMOOTH);
-	glClearColor (1,1,1,0.0);
+	glClearColor (0,0,0,0);
 	glMatrixMode (GL_PROJECTION);
 	glLoadIdentity ();
 	glEnable (GL_DEPTH_TEST);
@@ -265,52 +284,146 @@ reshape(int w,int h)
 void
 drawFloor()
 {
+	int xi=0;
+	int xy=0;
+	if (lost||win)
+	{
+		xi=-50;
+		xy=-50;
+		length=500;
+		width=500;
+		glBindTexture (GL_TEXTURE_2D,textures[8]);
+	}
+	else
+		glBindTexture (GL_TEXTURE_2D,textures[2]);
 	glPushMatrix ();
-	glBindTexture (GL_TEXTURE_2D,textures[2]);
 	glBegin (GL_QUADS);
-	glNormal3fv(normalUp);
+	glNormal3fv (normalUp);
 	glTexCoord2f (0.0f,0.0f);
-	glVertex3f (0,0,-h);
+	glVertex3f (xi,xy,-h);
 	glTexCoord2f (0.0f,1.0f);
-	glVertex3f (0,length,-h);
+	glVertex3f (xi,length,-h);
 	glTexCoord2f (1.0f,1.0f);
 	glVertex3f (width,length,-h);
 	glTexCoord2f (1.0f,0.0f);
-	glVertex3f (width,0,-h);
+	glVertex3f (width,xy,-h);
 	glEnd ();
 	glPopMatrix ();
 }
 void
 drawCeiling()
 {
+	int xi=0;
+	int xy=0;
+	if (lost||win)
+	{
+		xi=-50;
+		xy=-50;
+		length=500;
+		width=500;
+		glBindTexture (GL_TEXTURE_2D,textures[8]);
+	}
+	else
+		glBindTexture (GL_TEXTURE_2D,textures[0]);
 	glPushMatrix ();
-	glBindTexture (GL_TEXTURE_2D,textures[0]);
 	glBegin (GL_QUADS);
-	glNormal3fv(normalDown);
+	glNormal3fv (normalDown);
 	glTexCoord2f (0.0f,0.0f);
-	glVertex3f (0,0,h);
+	glVertex3f (xi,xy,h);
 	glTexCoord2f (0.0f,1.0f);
-	glVertex3f (0,length,h);
+	glVertex3f (xi,length,h);
 	glTexCoord2f (1.0f,1.0f);
 	glVertex3f (width,length,h);
 	glTexCoord2f (1.0f,0.0f);
-	glVertex3f (width,0,h);
+	glVertex3f (width,xy,h);
 	glEnd ();
 	glPopMatrix ();
+}
+void
+drawCube()
+{
+	float i=0;
+	float j=0;
+	float ic=c-1.2;
+	float jc=ic;
+	float tall=ic/2;
+	glBegin (GL_QUADS);
+	glNormal3fv (normalUp);
+	glTexCoord2f (1,0);
+	glVertex3f (j,i,tall);
+	glTexCoord2f (1,1);
+	glVertex3f (j,ic,tall);
+	glTexCoord2f (0,1);
+	glVertex3f (jc,ic,tall);
+	glTexCoord2f (0,0);
+	glVertex3f (jc,i,tall);
+	glNormal3fv (normalFront);
+	glTexCoord2f (1,1);
+	glVertex3f (jc,ic,tall);
+	glTexCoord2f (0,1);
+	glVertex3f (j,ic,tall);
+	glTexCoord2f (0,0);
+	glVertex3f (j,ic,-tall);
+	glTexCoord2f (1,0);
+	glVertex3f (jc,ic,-tall);
+	glNormal3fv (normalLeft);
+	glTexCoord2f (1,1);
+	glVertex3f (j,ic,tall);
+	glTexCoord2f (0,1);
+	glVertex3f (j,i,tall);
+	glTexCoord2f (0,0);
+	glVertex3f (j,i,-tall);
+	glTexCoord2f (1,0);
+	glVertex3f (j,ic,-tall);
+	glNormal3fv (normalFront);
+	glTexCoord2f (0,0);
+	glVertex3f (j,i,-tall);
+	glTexCoord2f (1,0);
+	glVertex3f (jc,i,-tall);
+	glTexCoord2f (1,1);
+	glVertex3f (jc,i,tall);
+	glTexCoord2f (0,1);
+	glVertex3f (j,i,tall);
+	glNormal3fv (normalRight);
+	glTexCoord2f (0,1);
+	glVertex3f (jc,ic,tall);
+	glTexCoord2f (1,1);
+	glVertex3f (jc,i,tall);
+	glTexCoord2f (1,0);
+	glVertex3f (jc,i,-tall);
+	glTexCoord2f (0,0);
+	glVertex3f (jc,ic,-tall);
+	glNormal3fv (normalDown);
+	glTexCoord2f (1,0);
+	glVertex3f (j,i,-tall);
+	glTexCoord2f (1,1);
+	glVertex3f (j,ic,-tall);
+	glTexCoord2f (0,1);
+	glVertex3f (jc,ic,-tall);
+	glTexCoord2f (0,0);
+	glVertex3f (jc,i,-tall);
+	glEnd ();
 }
 void
 positionRotatingCube(int& i,int& j)
 {
 	glPushMatrix ();
-	glColor3f (1,0,0);
-	glTranslatef (i*c,j*c,rimb);
+	glTranslatef ((i*c)+c/2,(j*c)+c/2,rimb);
 	glRotatef (rotate,0,0,1.6);
 	if (win)
-		glutSolidSphere (d,30,30);
+	{
+		glBindTexture (GL_TEXTURE_2D,textures[6]);
+		GLUquadric * q=gluNewQuadric ();
+		gluQuadricTexture (q,true);
+		gluSphere (q,sphereDim,200,200);
+	}
 	else
-		glutSolidCube (d);
+	{
+		glTranslatef (-((c-1.2)/2),-((c-1.2)/2),0);
+		glBindTexture (GL_TEXTURE_2D,textures[1]);
+		drawCube ();
+	}
 	glPopMatrix ();
-	glColor3f (0,1,0);
 }
 void
 rotateCube(int value)
@@ -318,15 +431,13 @@ rotateCube(int value)
 	if (win)
 	{
 		contatore++;
-		rimb-=0.13;
+		rimb-=0.20;
 		if (rimb<=-1)
 			rimb=1.1;
 	}
-	rotate+=1.0f;
+	rotate+=1.5f;
 	if (rotate>360)
-	{
 		rotate-=360;
-	}
 	glutPostRedisplay ();
 	if (contatore<500)
 		glutTimerFunc (25,rotateCube,0);
@@ -368,7 +479,7 @@ keyboard(int key,int x,int y)
 	{
 		int x=(int)(((vectorLookAt.eye[0])/c)+l*cos ((a*3.14)/180));
 		int y=(int)(((vectorLookAt.eye[1])/c)+l*sin ((a*3.14)/180));
-		if (moving (x,y)&&!aerial&&!lost)
+		if (moving (x,y)&&!win&&!lost)
 		{
 			vectorLookAt.eye[0]=vectorLookAt.eye[0]+l*cos ((a*3.14)/180);
 			vectorLookAt.eye[1]=vectorLookAt.eye[1]+l*sin ((a*3.14)/180);
@@ -381,7 +492,7 @@ keyboard(int key,int x,int y)
 	{
 		int x=(int)(((vectorLookAt.eye[0])/c)-l*cos ((a*3.14)/180));
 		int y=(int)(((vectorLookAt.eye[1])/c)-l*sin ((a*3.14)/180));
-		if (moving (x,y)&&!aerial&&!lost)
+		if (moving (x,y)&&!lost&&!win)
 		{
 			vectorLookAt.eye[0]=vectorLookAt.eye[0]-l*cos ((a*3.14)/180);
 			vectorLookAt.eye[1]=vectorLookAt.eye[1]-l*sin ((a*3.14)/180);
@@ -396,38 +507,30 @@ void
 keyPressed(unsigned char key,int x,int y)
 {
 	if (key=='f'||key=='F')
-	{
 		glutFullScreen ();
-		glutPostRedisplay ();
-	}
+	else if ((key=='z'||key=='Z')&&aerial)
+		zoom (true);
+	else if ((key=='x'||key=='X')&&aerial)
+		zoom (false);
+	else if (key=='l'||key=='L')
+		profVuoleVincere=false;
 	else if (key=='a'||key=='A')
 	{
 		if (!win&&!lost&&!aerial)
-		{
 			aerialWiew ();
-			glutPostRedisplay ();
-		}
 	}
 	else if (key=='s'||key=='S')
 	{
 		if (!win&&!lost&&aerial)
-		{
 			standardWiew ();
-			glutPostRedisplay ();
-		}
 	}
 	else if (key=='r'||key=='R')
-	{
 		reset ();
-		glutPostRedisplay ();
-	}
 	else if (key=='p'||key=='P')
-	{
 		profVuoleVincere=true;
-		glutPostRedisplay ();
-	}
 	else if (key==27)
 		exit (0);
+	glutPostRedisplay ();
 }
 void
 drawWall(int i,int j)
@@ -439,7 +542,7 @@ drawWall(int i,int j)
 	glBegin (GL_QUADS);
 	if (aerial)
 	{
-		glNormal3fv(normalUp);
+		glNormal3fv (normalUp);
 		glTexCoord2f (1,0);
 		glVertex3f (j,i,h);
 		glTexCoord2f (1,1);
@@ -449,7 +552,7 @@ drawWall(int i,int j)
 		glTexCoord2f (0,0);
 		glVertex3f (jc,i,h);
 	}
-	glNormal3fv(normalFront);
+	glNormal3fv (normalFront);
 	glTexCoord2f (1,1);
 	glVertex3f (jc,ic,h);
 	glTexCoord2f (0,1);
@@ -458,7 +561,7 @@ drawWall(int i,int j)
 	glVertex3f (j,ic,-h);
 	glTexCoord2f (1,0);
 	glVertex3f (jc,ic,-h);
-	glNormal3fv(normalLeft);
+	glNormal3fv (normalLeft);
 	glTexCoord2f (1,1);
 	glVertex3f (j,ic,h);
 	glTexCoord2f (0,1);
@@ -467,7 +570,7 @@ drawWall(int i,int j)
 	glVertex3f (j,i,-h);
 	glTexCoord2f (1,0);
 	glVertex3f (j,ic,-h);
-	glNormal3fv(normalFront);
+	glNormal3fv (normalFront);
 	glTexCoord2f (0,0);
 	glVertex3f (j,i,-h);
 	glTexCoord2f (1,0);
@@ -476,7 +579,7 @@ drawWall(int i,int j)
 	glVertex3f (jc,i,h);
 	glTexCoord2f (0,1);
 	glVertex3f (j,i,h);
-	glNormal3fv(normalRight);
+	glNormal3fv (normalRight);
 	glTexCoord2f (0,1);
 	glVertex3f (jc,ic,h);
 	glTexCoord2f (1,1);
@@ -493,54 +596,54 @@ display(void)
 	glClear (GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 	glMatrixMode (GL_MODELVIEW);
 	glPushMatrix ();
-	glColor3f (0,1,0);
 	gluLookAt (vectorLookAt.eye[0],vectorLookAt.eye[1],vectorLookAt.eye[2],vectorLookAt.direction[0],
 			vectorLookAt.direction[1],vectorLookAt.direction[2],vectorLookAt.up[0],vectorLookAt.up[1],
 			vectorLookAt.up[2]);
+	glMaterialfv (GL_FRONT,GL_SPECULAR,spec);
+	glMaterialfv (GL_FRONT,GL_SHININESS,sheen);
 	drawFloor ();
 	if (!aerial)
 		drawCeiling ();
-	else
+	else if (!win&&!lost)
 	{
-//		glPushMatrix ();
-//		glMaterialfv (GL_FRONT,GL_AMBIENT_AND_DIFFUSE,CubeColor1);
-//		glMaterialfv (GL_FRONT,GL_SPECULAR,spec);
-//		glMaterialfv (GL_FRONT,GL_SHININESS,sheen);
-//		glTranslatef ((saveX/5)*2,(saveY/5)*2,rimb);
-//		glutSolidCube (rimb);
-//		glPopMatrix ();
+		glPushMatrix ();
+		glTranslatef (vectorLookAt.eye[0],vectorLookAt.eye[1],rimb);
+		glRotatef (rotate,0,0,1.6);
+		glBindTexture (GL_TEXTURE_2D,textures[5]);
+		glTranslatef (-((c-1.2)/2),-((c-1.2)/2),0);
+		drawCube ();
+		glPopMatrix ();
 	}
 	for (int i=0;i<31;i++)
 	{
 		for (int j=0;j<38;j++)
 		{
-			if (map[i][j]==1)
+			if (map[i][j]==3)
 			{
-				if (i==0||j==0||i==30||j==37)
-				{
-					glPushMatrix ();
-					glBindTexture (GL_TEXTURE_2D,textures[4]);
-					drawWall (j,i);
-					glPopMatrix ();
-				}
+				glPushMatrix ();
+				if (aerial)
+					glBindTexture (GL_TEXTURE_2D,textures[3]);
 				else
-				{
-					if (!profVuoleVincere)
-					{
-						glPushMatrix ();
-						glBindTexture (GL_TEXTURE_2D,textures[3]);
-						drawWall (j,i);
-						glPopMatrix ();
-					}
-				}
+					glBindTexture (GL_TEXTURE_2D,textures[4]);
+				drawWall (j,i);
+				glPopMatrix ();
 			}
-//			else if (map[i][j]==2)
-//			{
-//				glPushMatrix ();
-//				glMaterialfv (GL_FRONT,GL_AMBIENT_AND_DIFFUSE,CubeColor);
-//				positionRotatingCube (i,j);
-//				glPopMatrix ();
-//			}
+			else if (map[i][j]==1&&!profVuoleVincere)
+			{
+				glPushMatrix ();
+				if (!lost)
+					glBindTexture (GL_TEXTURE_2D,textures[3]);
+				else
+					glBindTexture (GL_TEXTURE_2D,textures[7]);
+				drawWall (j,i);
+				glPopMatrix ();
+			}
+			else if (map[i][j]==2)
+			{
+				glPushMatrix ();
+				positionRotatingCube (i,j);
+				glPopMatrix ();
+			}
 		}
 	}
 	glPopMatrix ();
@@ -554,8 +657,8 @@ main(int argc,char **argv)
 	loadMap (random);
 	glutInit (&argc,argv);
 	glutInitDisplayMode (GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
-	glutInitWindowSize (1200,500);
-	glutCreateWindow ("IG-Project");
+	glutInitWindowSize (900,500);
+	glutCreateWindow ("Labirinth (project of IG)");
 	initializateVision ();
 	init ();
 	glutKeyboardFunc (keyPressed);
